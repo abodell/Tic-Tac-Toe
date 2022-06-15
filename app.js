da4ab5082e93ecedc6aec84dcd62dfb2f0e3928c
abodell <andybodell9@gmail.com>
2022-06-15 16:07:36 -0400

    Got the AI to make a move, but it is not selecting the correct space

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const scores = {
+    'X': 10,
+    'O': -10,
+    'tie': 0,
+};
+
 const Player = (marker) => {
     this.marker = marker;
     const getMarker = () => {
@@ -38,19 +44,31 @@ const gameBoard = (() => {
         }
     };
 
-    return {placeMarker, getPosition, clearBoard};
+    const getBoard = () => {
+        return board;
+    };
+
+    return {placeMarker, getPosition, clearBoard, getBoard};
 
 })();
 
 const gameController = (() => {
-    const player1 = Player('X', "");
-    const player2 = Player('O', "");
+    const player1 = Player('X'); // this will be the human player
+    const player2 = Player('O'); // this will be the ai
     const numToWin = 3;
 
     let isWinner = false;
     let isXTurn = true;
     let isTie = false;
+    let winningMarker;
 
+    const setWinningMarker = (marker) => {
+        winningMarker = marker;
+    };
+
+    const getWinningMarker = () => {
+        return winningMarker;
+    }
 
     const checkHorizWin = (row, col, marker) => {
         // need to traverse left and right to see how many in a row
@@ -154,7 +172,11 @@ const gameController = (() => {
     };
 
     const checkForWin = (row, col, marker) => {
-        return (checkHorizWin(row, col, marker) || checkVertWin(row, col, marker) || checkDiagWin(row, col, marker));
+        if ((checkHorizWin(row, col, marker) || checkVertWin(row, col, marker) || checkDiagWin(row, col, marker))) {
+            setWinningMarker(marker);
+            return true;
+        }
+        return false;
     };
 
     // need to check for ties
@@ -170,6 +192,7 @@ const gameController = (() => {
             return false;
         }
         isTie = true;
+        setWinningMarker('tie');
         return isTie;
     };
 
@@ -179,7 +202,64 @@ const gameController = (() => {
         isTie = false;
         gameBoard.clearBoard();
         displayController.changeMessage("Player " + player1.getMarker() + " it is your turn");
-    }
+    };
+
+    const bestMove = () => {
+        let bestScore = -Infinity;
+        let aiMove;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (gameBoard.getPosition(i, j) == '') {
+                    gameBoard.placeMarker(player2.getMarker(), i, j);
+                    let score = minimax(gameBoard.getBoard(), 0, false);
+                    gameBoard.placeMarker('', i, j);
+                    if (score > bestScore) {
+                        bestScore = score;
+                        aiMove = {i, j};
+                    }
+                }
+            }
+        }
+        return aiMove;
+    };
+
+    const minimax = (board, depth, isMaximizing) => {
+        let result = getWinningMarker();
+        if (result != null) {
+            return scores[result];
+        }
+        if (isMaximizing) {
+            let bestScore = -Infinity;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (gameBoard.getPosition(i, j) == '') {
+                        gameBoard.placeMarker(player2.getMarker(), i, j);
+                        checkForWin(i, j, player2.getMarker())
+                        let score = minimax(board, depth + 1, false);
+                        gameBoard.placeMarker('', i, j);
+                        bestScore = Math.max(score, bestScore);
+                    }
+                }
+            }
+            setIsWinner(false);
+            return bestScore;
+        } else {
+            let bestScore = Infinity;
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (gameBoard.getPosition(i, j) == '') {
+                        gameBoard.placeMarker(player1.getMarker(), i, j);
+                        checkForWin(i, j, player1.getMarker());
+                        let score = minimax(board, depth + 1, true);
+                        gameBoard.placeMarker('', i, j);
+                        bestScore = Math.min(score, bestScore);
+                    }
+                }
+            }
+            setIsWinner(false);
+            return bestScore;
+        }
+    };
 
     // logic for the gameplay
     const playerAction = (row, col) => {
@@ -194,12 +274,18 @@ const gameController = (() => {
                 displayController.changeMessage('Game Ended in a Tie!');
                 return;
             }
-
-            isXTurn = !isXTurn;
-            displayController.changeMessage("Player " + player2.getMarker() + " it is your turn");
+            let move = bestMove();
+            gameBoard.placeMarker(player2.getMarker(), move.i, move.j);
+            if (checkForWin(move.i, move.j, player2.getMarker())) {
+                displayController.changeMessage("The AI has won the game!");
+                return;
+            }
+            //displayController.changeMessage("Player " + player2.getMarker() + " it is your turn");
             
-        } else {
-            gameBoard.placeMarker(player2.getMarker(), row, col);
+        } /*else {
+            let choice = bestMove();
+            console.log(choice);
+            gameBoard.placeMarker(player2.getMarker(), choice.i, choice.j);
             if (checkForWin(row, col, player2.getMarker())) {
                 displayController.changeMessage("Player " + player2.getMarker() + " has won the game!");
                 return;
@@ -211,14 +297,22 @@ const gameController = (() => {
             }
             isXTurn = !isXTurn;
             displayController.changeMessage("Player " + player1.getMarker() + " it is your turn");
-        }
+        }*/
     };
 
     const getIsWinner = () => {
         return isWinner;
     }
 
-    return {playerAction, checkForWin, restartGame, getIsWinner};
+    const setIsWinner = (string) => {
+        isWinner = string;
+    };
+
+    const getIsXTurn = () => {
+        return isXTurn;
+    }
+
+    return {playerAction, checkForWin, restartGame, getIsWinner, getIsXTurn, bestMove};
 
 })();
 
@@ -227,7 +321,6 @@ const displayController = (() => {
     const spaces = document.querySelectorAll('.space');
     const message = document.querySelector('.message');
     const restartBtn = document.querySelector('#restart');
-    let gameMode;
 
     restartBtn.addEventListener('click', () => {
         gameController.restartGame();
@@ -237,9 +330,11 @@ const displayController = (() => {
     spaces.forEach((space) => {
         space.addEventListener('click', (event) => {
             // if the game is over or the space is already occupied, we don't want to allow a click
+            console.log(gameController.getIsWinner());
+            console.log(event.target.textContent);
             if (gameController.getIsWinner() || event.target.textContent !== '') {
                 return;
-            } else {
+            } else if (gameController.getIsXTurn()) {
                 gameController.playerAction(parseInt(event.target.dataset.row), parseInt(event.target.dataset.col));
                 populateDisplay();
             }
